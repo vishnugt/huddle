9c1bc9316a2f83d72f59b7b05f9a9e7b6e7876b7
Vishnu Gt <vishnugt@hotmail.com>
2019-10-19 20:07:53 +0530

    WIP OpenID login

--- src/main/java/in/huddleapp/Huddle/Utility/UrlConstants.java
+++ src/main/java/in/huddleapp/Huddle/Utility/UrlConstants.java
@@ -5,15 +5,15 @@ public class UrlConstants
 		//HOMEPAGE URLS
 		public static final String HOMEPAGE = "/";
 
-	//USER RELATED URLS
-	public static final String USER_BASE_URL = "/users";
-	public static final String ADD_USER = "/add";
-	public static final String ALL_USER = "/all";
+		//USER RELATED URLS
+		public static final String USER_BASE_URL = "/users";
+		public static final String ADD_USER = "/add";
+		public static final String ALL_USER = "/all";
 
-	//EVENT TYPE URLS
-	public static final String EVENT_TYPE_BASE_URL = "/eventtype";
-	public static final String MAJOR_ADD = "/major/add";
-	public static final String MAJOR_ALL = "/major/all";
-	public static final String MINOR_ADD = "/minor/add";
-	public static final String MINOR_ALL = "/minor/all";
+		//EVENT TYPE URLS
+		public static final String EVENT_TYPE_BASE_URL = "/eventType";
+		public static final String EVENT_MAJOR_ADD = "/major/add";
+		public static final String EVENT_MAJOR_ALL = "/major/all";
+		public static final String EVENT_MINOR_ADD = "/minor/add";
+		public static final String EVENT_MINOR_ALL = "/minor/all";
 }
